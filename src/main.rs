e3d47f942e53538e4ad5f8332bd25e54b9130ee2
l050101 <l01062506145@gmail.com>
2022-07-13 18:10:53 +0900

    hello

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // use std::fmt::format;
 use rocket::tokio::time::{sleep, Duration};
-
+use rocket::http::CookieJar;
 #[macro_use] extern crate rocket;
+
 #[get("/<one>/<two>")]
 fn param(one: &str, two: u8) -> String {
     format!("param test string : {}, integer : {}", one, two)
 }
 #[get("/")]
-async fn index() -> String {
+fn index() -> String {
     format!("home")
 }
 #[get("/<seconds>")]
@@ -15,10 +16,16 @@ async fn delay(seconds:u64) -> String{
     sleep(Duration::from_secs(seconds)).await;
     format!("delay {} seconds!",seconds)
 }
+#[get("/<_..>")]
+fn everything() -> &'static str {
+    "Hey, you're here."
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/",routes![index])
+        .mount("/every",routes![everything])
         .mount("/param", routes![param])
         .mount("/delay",routes![delay])
 }
